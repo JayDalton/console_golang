04b00a6c316b2b564dc4d891b9681ddf47ba132b
JayDalton <github@t-jay.net>
2022-10-27 14:05:59 +0200

    some data types

--- hello.go
+++ hello.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+const PI float64 = 3.14
+
 func main() {
 	fmt.Println("Hello, World!")
+
+	var Student1 string = "John"
+	var Student2 = "Jane"
+	number := 42
+
+	fmt.Println(Student1)
+	fmt.Println(Student2)
+	fmt.Println(number)
+
+	var a, b, c, d int = 1, 3, 5, 7
+	fmt.Println(a, b, c, d)
+
+	fmt.Println(PI)
+
 }
