4ff601cd551b864ce74e00e2345f30df47a282d2
JayDalton <github@t-jay.net>
2022-10-31 17:36:58 +0100

    add rest server by gin

--- hello.go
+++ hello.go
@@ -20,4 +20,17 @@ func main() {
 
 	fmt.Println(PI)
 
+	var arr1 = [3]int{3,5,7}
+	arr2 := [5]int{2,4,6,8,0}
+	arr3 := [5]int{1:10,2:20}
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+	fmt.Println(len(arr3))
+	fmt.Println(arr2[0])
+
+	var cars = [4]string{"Volvo", "BMW", "Audi", "Ford"}
+	fmt.Println(cars)
+
+
 }
